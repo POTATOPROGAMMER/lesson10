f9caea633f5b3d49a6ff3aea07bd4a85b0fb33ad
POTATOPROGAMMER <codercode1010@gmail.com>
2024-12-07 12:18:16 +0400

    commit1

---
+++ activity1.py
@@ -0,0 +1,26 @@
+pen="welcome"
+for i in pen:
+    print(i)
+
+#print0to10
+for i in range(15):
+    print (i)
+#print0to20
+for zayed in range(21):
+    print(zayed)
+
+#print0,10
+for z in range(1,16):
+    print(z)
+#print0,10
+for z in range(0,12,2):
+    print(z)
+#print0,11
+for z in range(1,12,2):
+    print(z)
+#print10,0
+for z in range(10,0,-1):
+    print(z)
+#print-1,-11
+for z in range(-1,-11,-1):
+    print(z)
